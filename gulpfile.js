f611669ea50904b4967f3a50ba292f255f3ba44b
imvarunkmr <imvarunkmr@gmail.com>
2017-01-12 12:17:44 +0530

    Changed CSS output style from compact to expanded

--- gulpfile.js
+++ gulpfile.js
@@ -172,10 +172,10 @@ gulp.task( 'browser-sync', function() {
  		.pipe( sourcemaps.init() )
  		.pipe( sass( {
  			errLogToConsole: true,
- 			outputStyle: 'compact',
+ 		// 	outputStyle: 'compact',
  			//outputStyle: 'compressed',
  			// outputStyle: 'nested',
- 			// outputStyle: 'expanded',
+ 			outputStyle: 'expanded',
  			precision: 10
  		} ) )
  		.on('error', console.error.bind(console))
